be75c19810f90103b7972f7bcb9477edd7a1db60
ilaganf <ilaganf@stanford.edu>
2018-05-09 21:50:38 -0700

    cleaned up style and replaced correlation with numpy spearman correlation, everything still looks like it works

--- Model.py
+++ Model.py
@@ -28,7 +28,6 @@ class Model():
             self.build()
             tf.summary.scalar("loss", self.loss)
             tf.summary.scalar("grads norm", self.grad_norm)
-            tf.summary.scalar("pearson correlation", self.corr)
             self.merged = tf.summary.merge_all()
             self.init_op = tf.global_variables_initializer()
             self.saver = tf.train.Saver()
@@ -40,7 +39,6 @@ class Model():
         self.pred = self.add_prediction_op()
         self.loss = self.add_loss_op(self.pred)
         self.train_op = self.add_training_op(self.loss)
-        self.corr = self.corr(self.pred)
 
 
     def add_placeholders(self):
@@ -64,28 +62,6 @@ class Model():
             device_count={'CPU': 1})
         self.sess = tf.Session(config=session_conf, graph = self.graph)
         self.sess.run(self.init_op)
-
-
-    def format_dataset(self, dataset, n=config.NUM_CELLS_IN_DATASET):
-        dsets = list(dataset.index.unique())
-        X = np.zeros((len(dsets), n, self.config.n_features))
-        y = np.zeros((len(dsets), n, 1))
-        for i, dset in enumerate(dsets):
-            data = dataset.loc[dset]
-            data = data.sample(n=n, replace=True)
-            X[i] = data.loc[:, data.columns != "Standardized_Order"].as_matrix()
-            y[i] = np.matrix(data["Standardized_Order"].as_matrix()).T
-        return X, y
-        
-        
-    def create_feed_dict(self, inputs_batch, labels_batch=None, dropout=0):
-        '''
-        Creates the model feed dict
-        '''
-        feed_dict = {self.input_placeholder: inputs_batch, self.dropout_placeholder: dropout}
-        if labels_batch is not None:
-            feed_dict[self.labels_placeholder] = labels_batch
-        return feed_dict
 
 
     def add_prediction_op(self):
@@ -118,8 +94,89 @@ class Model():
         combined = gcs * sims * combined_weight
         temp = tf.concat([self.input_placeholder[:, :, 0:1], combined], axis=2)
         return tf.concat([temp, self.input_placeholder[:, :, end:]], axis=2)
+
+
+    def add_loss_op(self, pred):
+        # squared loss
+        loss = self.config.beta * tf.losses.mean_squared_error(self.labels_placeholder, pred)
+        self.squared = tf.losses.mean_squared_error(self.labels_placeholder, 
+                                                    pred, loss_collection=None)
+
+        # L2 regularization
+        weights = [var for var in tf.trainable_variables() if 'weights' in str(var)]
+        l2 = np.sum([tf.nn.l2_loss(var) for var in weights]) 
+        loss += self.config.lambd / 2 * l2
+            
+        return loss
+
+
+    def add_training_op(self, loss):
+        optimizer = tf.train.AdamOptimizer(self.config.lr)
+        grads_and_vars = optimizer.compute_gradients(self.loss)
+        grads = [gv[0] for gv in grads_and_vars]
+        vars = [gv[1] for gv in grads_and_vars]
+        if self.config.grad_clip:
+            grads, _ = tf.clip_by_global_norm(grads, self.config.clip_val)
+        grads_and_vars = zip(grads, vars)
+        self.grad_norm = tf.global_norm(grads)
+        train_op = optimizer.apply_gradients(grads_and_vars)
+        return train_op
+
+
+    def fit(self, train_examples, dev_set):
+        best_dev = float("inf")
+        epoch = 0
+        while epoch < self.config.n_epochs:
+            epoch += 1
+            if self.verbose:
+                print("Epoch {:} out of {:}".format(epoch, self.config.n_epochs))
+            dev_squared = self.run_epoch(pd.concat([train_examples, dev_set]), epoch)
+            if dev_squared < best_dev:
+                epoch = 0
+                best_dev = dev_squared
+                if self.saver:
+                    if self.verbose:
+                        print("New best dev MSE! Saving model in ./results/model.weights/weights")
+                    self.saver.save(self.sess, self.config.model_output)
+            if self.verbose: print()
+
+        return epoch
+
+
+    def run_epoch(self, train_data, index):
+        train_x, train_y = self.format_dataset(train_data)
+        loss = self.train(train_x, train_y)
+        
+        train_squared, pred = self.test(train_data)
+        corr, _ = scipy.stats.spearmanr(pred.flatten(), train_y.flatten())
+        if self.verbose:
+            print("train corr:", corr, " train squared:", train_squared)
+
+        return train_squared
+
     
-    
+    def format_dataset(self, dataset, n=config.NUM_CELLS_IN_DATASET):
+        dsets = list(dataset.index.unique())
+        X = np.zeros((len(dsets), n, self.config.n_features))
+        y = np.zeros((len(dsets), n, 1))
+        for i, dset in enumerate(dsets):
+            data = dataset.loc[dset]
+            data = data.sample(n=n, replace=True)
+            X[i] = data.loc[:, data.columns != "Standardized_Order"].as_matrix()
+            y[i] = np.matrix(data["Standardized_Order"].as_matrix()).T
+        return X, y
+
+
+    def create_feed_dict(self, inputs_batch, labels_batch=None, dropout=0):
+        '''
+        Creates the model feed dict
+        '''
+        feed_dict = {self.input_placeholder: inputs_batch, self.dropout_placeholder: dropout}
+        if labels_batch is not None:
+            feed_dict[self.labels_placeholder] = labels_batch
+        return feed_dict
+
+
     def train(self, inputs_batch, labels_batch):
         '''
         Performs a single training iteration on the given
@@ -128,7 +185,8 @@ class Model():
         feed = self.create_feed_dict(inputs_batch=inputs_batch, 
                                      labels_batch=labels_batch,
                                      dropout=self.config.dropout)
-        _, loss, summary = self.sess.run([self.train_op, self.loss, self.merged], feed_dict=feed)
+        _, loss, summary = self.sess.run([self.train_op, self.loss, self.merged], 
+                                          feed_dict=feed)
         return loss
 
     
@@ -140,57 +198,14 @@ class Model():
         input_dsets = list(data.index.unique())
         dataset_x, dataset_y = self.format_dataset(data)
         feed = self.create_feed_dict(dataset_x, dataset_y, dropout=self.config.dropout)
-        corr, squared, pred = self.sess.run([self.corr, self.squared, self.pred], feed_dict = feed)
-        return corr, squared, pred 
-    
-
-    def run_epoch(self, train_data, index):
-        train_x, train_y = self.format_dataset(train_data)
-        loss = self.train(train_x, train_y)
-        
-        train_corr, train_squared, pred = self.test(train_data)
-        if self.verbose:
-            print("train corr:", train_corr, "train squared:", train_squared)
-        return train_corr, train_squared
-
-    
-    def fit(self, train_examples, dev_set):
-        best_dev = float("inf")
-        epoch = 0
-        while epoch < self.config.n_epochs:
-            epoch += 1
-            if self.verbose:
-                print("Epoch {:} out of {:}".format(epoch, self.config.n_epochs))
-            dev_corr, dev_squared = self.run_epoch(pd.concat([train_examples, dev_set]), epoch)
-            # 
-            if dev_squared < best_dev:
-                epoch = 0
-                best_dev = dev_squared
-                if self.saver:
-                    if self.verbose:
-                        print("New best dev MSE! Saving model in ./results/model.weights/weights")
-                    self.saver.save(self.sess, self.config.model_output)
-                    
-            if self.verbose: print()
-        return epoch
+        squared, pred = self.sess.run([self.squared, self.pred], feed_dict=feed)
+        return squared, pred 
 
     
     def evaluate(self, data):
         self.saver.restore(self.sess, self.config.model_output)
-        corr, squared, pred = self.test(data)
-        return corr, squared, pred
-
-
-    def corr(self, pred):
-        vx = tf.squeeze(pred) - tf.reduce_mean(pred, axis = 1)
-        vy = tf.squeeze(self.labels_placeholder) - tf.reduce_mean(self.labels_placeholder, axis = 1)
-        corr_num = tf.reduce_sum(tf.multiply(vx, vy), axis=1)
-        corr_den = tf.sqrt(tf.multiply(tf.reduce_sum(tf.square(vx), axis=1), tf.reduce_sum(tf.square(vy), axis=1)))
-        corr = corr_num/corr_den
-        self.squared = tf.losses.mean_squared_error(self.labels_placeholder, pred)
-        corr = tf.reduce_mean(corr)
-        self.pred = pred
-        return corr
+        squared, pred = self.test(data)
+        return squared, pred
 
 
     def make_pred(self, data):
@@ -209,34 +224,7 @@ class Model():
             pred = np.squeeze(pred)
             preds.append(pred)
         pred = np.concatenate(preds)[0:init_len]
-        return pred
-    
-
-    def add_loss_op(self, pred):
-       
-        # squared loss
-        loss = self.config.beta * tf.losses.mean_squared_error(self.labels_placeholder, pred)
-
-        # L2 regularization
-        weights = [var for var in tf.trainable_variables() if 'weights' in str(var)]
-        l2 = np.sum([tf.nn.l2_loss(var) for var in weights]) 
-        loss += self.config.lambd / 2 * l2
-            
-        return loss
-
-    def add_training_op(self, loss):
-        optimizer = tf.train.AdamOptimizer(self.config.lr)
-        grads_and_vars = optimizer.compute_gradients(self.loss)
-        grads = [gv[0] for gv in grads_and_vars]
-        vars = [gv[1] for gv in grads_and_vars]
-        if self.config.grad_clip:
-            grads, _ = tf.clip_by_global_norm(grads, self.config.clip_val)
-        grads_and_vars = zip(grads, vars)
-        self.grad_norm = tf.global_norm(grads)
-        train_op = optimizer.apply_gradients(grads_and_vars)
-        return train_op
-    
-
+        return pred 
 
 
     def save(self):
@@ -247,7 +235,6 @@ class Model():
             os.makedirs(self.config.model_output)
 
         self.saver.save(self.sess, self.config.model_output)
-        
         
         
 def main():
