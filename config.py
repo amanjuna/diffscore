698cee4ba5290bc73ce953145f2f9fd72297142f
ilaganf <ilaganf@stanford.edu>
2018-05-10 17:46:51 -0700

    doop de doop

--- config.py
+++ config.py
@@ -60,9 +60,9 @@ INDROP = list(set(["Camargo", "DirectProtocol", "StandardProtocol"]).intersectio
 
 
 class Config(object):
-    def __init__(self, n_features=108, n_neighbors=50, n_classes=1, dropout=0.0, n_layers=4,
-                 hidden_size=50, n_epochs=100, lr=0.01, alpha=0, beta=1, 
-                 lambd=0.01, grad_clip=False, clip_val=10, crossval=0):
+    def __init__(self, n_features=108, n_neighbors=50, n_classes=1, dropout=0.0, n_layers=3,
+                 hidden_size=100, n_epochs=100, lr=0.01, alpha=0, beta=1, 
+                 lambd=1, grad_clip=False, clip_val=10, crossval=0):
         attributes = {}
         self.n_features = n_features
         self.n_neighbors = n_neighbors
