084d4abc7c07ee581327a3fc8ff48c0ea2fc6a09
ilaganf <ilaganf@stanford.edu>
2018-05-17 19:42:20 -0700

    tf.data.Dataset has come to play

--- Model.py
+++ Model.py
@@ -17,28 +17,54 @@ import visualize
 
 class Model():
 
-    def __init__(self, config, verbose=True):
+    def __init__(self, config, data, is_training=True, verbose=True):
         self.config = config
-        self.build_graph()
+        self.build_graph(data, is_training)
         self.verbose = verbose
 
 
-    def build_graph(self):
+    def build_graph(self, data, is_training):
         with tf.Graph().as_default() as self.graph:
-            self.build()
-            tf.summary.scalar("loss", self.loss)
-            tf.summary.scalar("grads norm", self.grad_norm)
-            self.merged = tf.summary.merge_all()
+            self.build(data, is_training)
+            if is_training:
+                tf.summary.scalar("loss", self.loss)
+                tf.summary.scalar("grads norm", self.grad_norm)
+                self.merged = tf.summary.merge_all()
             self.init_op = tf.global_variables_initializer()
             self.saver = tf.train.Saver()
         self.graph.finalize()
 
 
-    def build(self):
-        self.add_placeholders()
+    def build(self, data, is_training):
+        # self.add_placeholders()
+        self.input, self.iter_init = self.format_dataset(data)
         self.pred = self.add_prediction_op()
         self.loss = self.add_loss_op(self.pred)
-        self.train_op = self.add_training_op(self.loss)
+        if is_training:
+            self.train_op = self.add_training_op(self.loss)
+
+
+    def format_dataset(self, data, n=config.NUM_CELLS_IN_DATASET):
+        cols = list(data.columns)
+        cols.remove('Standardized_Order')
+        cols.remove('weight')
+
+        inputs = tf.constant(data.loc[:, cols].as_matrix(), tf.float32)
+        weights = np.matrix(data["weight"].as_matrix()).T
+        weights *= weights.shape[0]/np.sum(weights)
+        weights = tf.constant(weights, tf.float32)
+        labels = tf.constant(np.matrix(data["Standardized_Order"].as_matrix()).T, tf.float32)
+
+        dataset = tf.data.Dataset.from_tensor_slices((inputs, weights, labels))
+        dataset = dataset.shuffle(10000).batch(self.config.batch_size)
+
+        iterator = dataset.make_initializable_iterator()
+        cells = iterator.get_next()
+        iter_initializer = iterator.initializer
+
+        self.data_len = len(data)
+
+        return cells, iter_initializer
 
 
     def add_placeholders(self):
@@ -50,6 +76,7 @@ class Model():
                                                  name = "output")
         self.dropout_placeholder = tf.placeholder(tf.float32)
         self.weight_placeholder = tf.placeholder(tf.float32, shape=[None, 1])
+
 
     def initialize(self):
         '''
@@ -63,6 +90,7 @@ class Model():
         self.sess = tf.Session(graph = self.graph)
         self.sess.run(self.init_op)
 
+
     def corr(self, pred):
         vx = pred - tf.reduce_mean(pred, axis = 0)
         vy = self.labels_placeholder - tf.reduce_mean(self.labels_placeholder, axis = 0)
@@ -73,17 +101,19 @@ class Model():
         self.squared = tf.losses.mean_squared_error(self.labels_placeholder, pred)
         return corr	        
 
+
     def add_prediction_op(self):
         '''
         Defines the computational graph then returns the prediction tensor
         '''
-        x = self.input_placeholder#self.combine_features()
-        arr = [0]*(self.config.n_layers+1)
-        arr[0] = tf.contrib.layers.layer_norm(x)
-        for i in range(1, self.config.n_layers+1):
-            arr[i] = tf.contrib.layers.fully_connected(arr[i-1], self.config.hidden_size)
-            arr[i] = tf.contrib.layers.layer_norm(arr[i])
-        output = tf.contrib.layers.fully_connected(arr[self.config.n_layers], 1, activation_fn=None)
+        with tf.variable_scope('predictions', reuse=tf.AUTO_REUSE):
+            x = self.input[0] # input data (self.input is a (cell, weight, label) tuple)
+            arr = [0]*(self.config.n_layers+1)
+            arr[0] = tf.contrib.layers.layer_norm(x)
+            for i in range(1, self.config.n_layers+1):
+                arr[i] = tf.contrib.layers.fully_connected(arr[i-1], self.config.hidden_size)
+                arr[i] = tf.contrib.layers.layer_norm(arr[i])
+            output = tf.contrib.layers.fully_connected(arr[self.config.n_layers], 1, activation_fn=None)
         return output
 
 
@@ -107,16 +137,19 @@ class Model():
 
 
     def add_loss_op(self, pred):
-        # squared loss
-        loss = self.config.beta * tf.losses.mean_squared_error(self.labels_placeholder, pred, weights=self.weight_placeholder)
-        self.squared = tf.losses.mean_squared_error(self.labels_placeholder, 
-                                                    pred, weights=self.weight_placeholder,
-                                                    loss_collection=None)
 
-        # L2 regularization
-        weights = [var for var in tf.trainable_variables() if 'weights' in str(var)]
-        l2 = np.sum([tf.nn.l2_loss(var) for var in weights]) 
-        loss += self.config.lambd / 2 * l2
+        with tf.variable_scope("loss", reuse=tf.AUTO_REUSE):
+            # squared loss
+            # recall that self.input is a (cell, weight, label) tuple
+            loss = self.config.beta * tf.losses.mean_squared_error(self.input[2], pred, weights=self.input[1])
+            self.squared = tf.losses.mean_squared_error(self.input[2], 
+                                                        pred, weights=self.input[1],
+                                                        loss_collection=None)
+
+            # L2 regularization
+            weights = [var for var in tf.trainable_variables() if 'weights' in str(var)]
+            l2 = np.sum([tf.nn.l2_loss(var) for var in weights]) 
+            loss += self.config.lambd / 2 * l2
             
         return loss
 
@@ -134,50 +167,40 @@ class Model():
         return train_op
 
 
-    def fit(self, train_examples, dev_set):
-        print(train_examples, dev_set)
-        best_dev = float("inf")
+    def fit(self):
+        best_loss = float("inf")
         epoch = 0
         while epoch < self.config.n_epochs:
             epoch += 1
             if self.verbose:
                 print("Epoch {:} out of {:}".format(epoch, self.config.n_epochs))
-            dev_squared = self.run_epoch(pd.concat([train_examples, dev_set]), epoch)
-            if dev_squared < best_dev:
-                best_dev = dev_squared
+            loss = self.train_epoch(epoch)
+            if loss < best_loss:
+                best_loss = loss
                 if self.saver:
                     if self.verbose:
-                        print("New best dev MSE! Saving model in ./results/model.weights/weights")
+                        print("New best MSE! Saving model in ./results/model.weights/weights")
                     self.saver.save(self.sess, self.config.model_output)
             if self.verbose: print()
 
         return epoch
 
 
-    def run_epoch(self, train_data, index):
-        train_x, train_y, weight = self.format_dataset(train_data)
-        loss = self.train(train_x, train_y, weight) 
-        train_squared, pred = self.test(train_data)
-        if index % 100 == 0:
-            corr, _ = scipy.stats.spearmanr(pred, train_y)
-        else:
-            corr = 0
-        #corr = 0
-        if self.verbose:
-            print("train corr:", corr, " train squared:", train_squared)
+    def train_epoch(self, index):
+        '''
+        Performs a training epoch
+        '''
+        print("\nEpoch %d"%index)
+        self.sess.run(self.iter_init)
 
-        return train_squared
+        num_steps = (self.data_len + self.config.batch_size - 1) // self.config.batch_size
+        for i in range(num_steps):
+            _, loss, summary = self.sess.run([self.train_op, self.loss, self.merged])
+            
+            if self.verbose and i % 10 == 0:
+                print("Iteration {} loss: {}".format(i, loss))
 
-    
-    def format_dataset(self, data, n=config.NUM_CELLS_IN_DATASET):
-        cols = list(data.columns)
-        cols.remove('Standardized_Order')
-        cols.remove('weight')
-        X = data.loc[:, cols].as_matrix()
-        y = np.matrix(data["Standardized_Order"].as_matrix()).T
-        weight = np.matrix(data["weight"].as_matrix()).T
-        weight *= weight.shape[0]/np.sum(weight)
-        return X, y, weight
+        return loss
 
 
     def create_feed_dict(self, inputs_batch, labels_batch=None, dropout=0, weight=None):
@@ -190,20 +213,6 @@ class Model():
         if weight is not None:
             feed_dict[self.weight_placeholder] = weight
         return feed_dict
-
-
-    def train(self, inputs_batch, labels_batch, weight):
-        '''
-        Performs a single training iteration on the given
-        input batch.
-        '''
-        feed = self.create_feed_dict(inputs_batch=inputs_batch, 
-                                     labels_batch=labels_batch,
-                                     dropout=self.config.dropout,
-                                     weight=weight)
-        _, loss, summary = self.sess.run([self.train_op, self.loss, self.merged], 
-                                          feed_dict=feed)
-        return loss
 
     
     def test(self, data):
@@ -224,15 +233,20 @@ class Model():
         return squared, pred
 
 
-    def make_pred(self, data):
+    def make_pred(self):
         self.saver.restore(self.sess, self.config.model_output)
-        cols = list(data.columns)
-        cols.remove('weight')
-        X = data.ix[:, cols].as_matrix()
-        feed = self.create_feed_dict(X)
-        pred = self.sess.run(self.pred, feed)
-        pred = np.squeeze(pred)
-        return pred 
+        # cols = list(data.columns)
+        # cols.remove('weight')
+        # X = data.ix[:, cols].as_matrix()
+        # feed = self.create_feed_dict(X)
+        num_steps = (self.data_len + self.config.batch_size - 1) // self.config.batch_size
+        self.sess.run(self.iter_init)
+        preds = np.array([])
+        for _ in range(num_steps):
+            pred = self.sess.run(self.pred)
+            pred = np.squeeze(pred)
+            np.concatenate((preds, pred))
+        return preds
 
 
     def save(self):
@@ -249,25 +263,25 @@ def main():
     tf.set_random_seed(1234)
     param = config.Config(hidden_size=300,
                           n_layers=3, 
-                          n_epochs=1000,  
+                          n_epochs=1,  
                           beta=1, 
                           lambd=0, 
                           lr=5e-5)
     
     # train, dev, test, dsets = preprocessing.load_data(model_path=param.output_path)
     all_data = preprocessing.load_data(model_path=param.output_path, separate=False)
-    all_data = pd.read_csv("data/unified_processed.csv").sample(1000)
+    all_data = pd.read_csv("data/unified_processed.csv")
     all_data = all_data.loc[:,"Standardized_Order":"weight"]
     plate = all_data.loc[(all_data.Plate==1.0) | (all_data.C1==1.0)]
     nonplate = all_data.loc[(all_data.Plate==0) & (all_data.C1==0)]
 
     # Fit and log model
-    model = Model(param)
+    model = Model(param, all_data, is_training=True)
     model.initialize()
-    model.fit(all_data, pd.DataFrame())
-    visualize.model_prediction_plot(model, all_data)
-    visualize.model_prediction_plot(model, plate, './plots/model_predictions_plate.png')
-    visualize.model_prediction_plot(model, nonplate, './plots/model_prediction_nonplate.png')
+    model.fit()
+    visualize.model_prediction_plot(param, all_data)
+    visualize.model_prediction_plot(param, plate, './plots/model_predictions_plate.png')
+    visualize.model_prediction_plot(param, nonplate, './plots/model_prediction_nonplate.png')
     # model.sess.close()
 
 if __name__ == "__main__":
