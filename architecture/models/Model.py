eecf8f57e813cc48725328bfcc7525bca9e04c64
ilaganf <ilaganf@stanford.edu>
2018-05-31 23:16:37 -0700

    trying to fix bug where loading previous weights doesn't happen correctly

--- architecture/models/Model.py
+++ architecture/models/Model.py
@@ -100,7 +100,7 @@ class Model():
                 if self.verbose: print()
 
 
-    def predict(self, data):
+    def predict(self, data, restore=True):
         '''
         @data is a pandas dataframe
         '''
@@ -109,12 +109,16 @@ class Model():
         iterator = inputs.make_initializable_iterator()
         
         self.input_data = iterator.get_next()
-        self.build()
+
+        if restore:
+            self.build()
+
         self.handle = tf.placeholder(tf.string, shape=())
         
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
-            self.saver.restore(sess, self.config.model_output)
+            if restore:
+                self.saver.restore(sess, self.config.model_output)
             test_handle = sess.run(iterator.string_handle())
             sess.run(iterator.initializer)
             preds = []
@@ -125,6 +129,7 @@ class Model():
                     preds.append(_pred)
                 except tf.errors.OutOfRangeError:
                     break
+
         return np.concatenate(preds).flatten()
 
     
